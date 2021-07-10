d68e413d60e7d864740eaeaa18babcf1372c2dd6
Mitch Dzugan <mitchdzugan@gmail.com>
2021-07-09 17:12:06 -0700

    node start

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const { spawn } = require("child_process");
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/start', (req, res) => {
+	const proc = spawn("ruby", ["./basics.rb"]);
+	proc.stdout.on("data", data => {
+		console.log({ data });
+	});
+  res.send('Hello World!!!');
 });
 
 app.listen(port, () => {
