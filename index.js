055ff2a21f4273ee3f09e34510fb9bd5bcf84cb4
Mitch Dzugan <mitchdzugan@gmail.com>
2021-07-09 17:03:09 -0700

    fix

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 5000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
 
